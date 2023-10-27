5290de6cc49f218285331f1782aecfa3a4e6336a
Oyatulloh-Isroilov <oyatullohisroilov5@gmail.com>
2023-10-27 20:52:54 +0500

    4-oy 10-dars

---
+++ app.js
@@ -0,0 +1,75 @@
+//
+// 1-masala
+//
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let arr1 = arr.filter(function (value) {
+//   return value % 2 == 1;
+// });
+// console.log(arr1);
+
+//
+// 2-masala
+//
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let arr1 = arr.map(function (value) {
+//   if (value % 2 == 1) {
+//     return value ** 2;
+//   } else if (value % 2 == 0) {
+//     return value ** 3;
+//   }
+// });
+
+// console.log(arr1);
+
+//
+// 3-masala
+//
+// let arr = [1, 2, 6, 15, 20, 30, 7, 8, 9, 45];
+
+// let arr1 = arr.some(function (value) {
+//   if (value % 3 == 0 && value % 5 == 0) {
+//     return "3ga va 5ga bo'linadigan sonlar mavjud";
+//   } else {
+//     return "3ga va 5ga bo'linadigan sonlar mavjud emas";
+//   }
+// });
+// localStorage.setItem("3ga va 5ga bo'linadigan sonlar", arr1);
+// console.log(localStorage.getItem("3ga va 5ga bo'linadigan sonlar"));
+//
+// 4-masala
+//
+
+// let arr = [
+//   "olma",
+//   "nok",
+//   "uzum",
+//   "banan",
+//   "gilos",
+//   "olxori",
+//   "malina",
+//   "qulupnay",
+//   "anor",
+//   "qovun",
+// ];
+
+// let arr1 = function (value) {
+//   let value = arr.charAt(1);
+// };
+
+// console.log(arr1);
+//
+// 5-masala
+//
+// let arr = [1234567890];
+// let arr1 = arr.join(",");
+// console.log(arr1);
+
+//
+// 6-masala
+//
+// let letter = "salom bolalar";
+
+// letter = letter.replace("r", "p");
+// console.log(letter);
